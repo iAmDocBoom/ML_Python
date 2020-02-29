13fc403c2669bd4347ab6464ec0a838f63350bad
Soutrik <soutrikmaiti.uta@gmail.com>
2020-02-29 15:54:02 -0800

    Features and Labels

---
+++ regression_features_labels.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import math
+import quandl
+
+df = quandl.get('WIKI/GOOGL')
+df = df[['Adj. Open','Adj. High','Adj. Low','Adj. Close','Adj. Volume']]
+df['HL_PCT'] = (df['Adj. High'] - df['Adj. Close']) / df['Adj. Open'] * 100.0
+df['PCT_Change'] = (df['Adj. Close'] - df['Adj. Open']) / df['Adj. Open'] * 100.0
+
+df = df[['Adj. Close', 'HL_PCT','PCT_Change','Adj. Volume']] #features
+
+forecast_col = 'Adj. Close'
+df.fillna('-99999',inplace=True)
+
+forecast_out = int(math.ceil(0.01*len(df)))  #10 percent of the data
+
+df['label'] = df[forecast_col].shift(-forecast_out)
+df.dropna(inplace=True)
+
+print(df.head())
