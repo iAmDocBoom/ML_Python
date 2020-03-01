cb1e3e98d9364110d85cc37e7e6705052b8deb91
Soutrik <soutrikmaiti.uta@gmail.com>
2020-02-29 17:45:12 -0800

    Extra files

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,8 @@
+
+# X_train, X_test, Y_train, Y_test= model_selection.train_test_split(X,Y, test_size=0.2)
+# clf = LinearRegression(n_jobs=-1)
+# # clf = svm.SVR(kernel='poly')
+# clf.fit(X_train,Y_train)
+
+# with open('linearregression.pickle','wb') as f:
+#     pickle.dump(clf,f)
